5654a0f1789202471fb53eb118152d3ccb14e92c
Hakan Bostan <wua663@haw-hamburg.de>
2024-07-28 22:46:22 +0200

    Hakan Bostan, Cpt CC A233 FinalV.,28.07.2024

--- captainciaociao/src/main/java/kapitelzwei/uebungZweiDreiDrei.java
+++ captainciaociao/src/main/java/kapitelzwei/uebungZweiDreiDrei.java
@@ -11,11 +11,11 @@ public class uebungZweiDreiDrei {
      * @return boolean.
      */
 
-    public boolean ausgabe6() {
+     public boolean ausgabe6() {
 
         System.out.println("gib 2 Zahlen zwischen 0 und 99 an");
         String x = new Scanner(System.in).nextLine();
-        String y = new Scanner(System.in).nextLine();    
+        String y = new Scanner(System.in).nextLine();
 
         for (int i = 0; i <= 2; i++) {
 
@@ -24,29 +24,28 @@ public class uebungZweiDreiDrei {
                 char digit1 = x.charAt(x.length()-1);
                 int temp1 = -1;
 
-                if(digit1 == y.charAt(y.length()- temp1)){
+                if(digit1 == y.charAt(y.length() + temp1)){
                     System.out.println( x + " und " + y + " haben eine gemeinsame Ziffer");
                     return true;
-                };
-
-                temp1 -= 1;
+                } else
+                    temp1 += -1;
             }
 
             char digit2 = x.charAt(x.length()-2);
             int temp2 = -1;
-            if (digit2 == y.charAt(y.length()-temp2)) {
-                
+            if (digit2 == y.charAt(y.length()+ temp2)) {
+
                 System.out.println( x + " und " + y + " haben eine gemeinsame Ziffer");
                 return true;
 
             }
-            temp2 -= 1;
+            temp2 += 1;
 
         }
         System.out.println( x + " und " + y + " haben keine gemeinsame Ziffer");
         return false;
 
-    
+
 
     }
 
