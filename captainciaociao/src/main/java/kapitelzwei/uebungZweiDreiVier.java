fda35b1977cc378c282bc5683566caac4335c5c4
Hakan Bostan <wua663@haw-hamburg.de>
2024-07-29 23:15:35 +0200

    Hakan Bostan, Cpt CC A234 (erste Ideen), 29.07.2024

---
+++ captainciaociao/src/main/java/kapitelzwei/uebungZweiDreiVier.java
@@ -0,0 +1,29 @@
+package kapitelzwei;
+
+import java.util.Scanner;
+
+public class uebungZweiDreiVier {
+
+    
+     /**
+      
+     * Die Methode liest einen Wert von der Konsole ein und berechnet 
+     * mit wie vielen Euro und Cent Stücken der Betrag zu standekommt.
+     * 
+     * @param n.A.
+     * @return n.A.
+     */
+
+  public void ausgabe7() {
+
+        System.out.println("gib einen Geldbetrag an.");
+        float fvalue = new Scanner(System.in).nextFloat();
+
+        int vorKomma = (int) fvalue;
+        float nachKomma = fvalue - vorKomma;
+
+        // Mit Modulo Operator rest werte bestimmen
+
+    }
+
+}
