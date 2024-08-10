e3fb28c5e1d259900a894539633c09ab89a83839
Hakan Bostan <wua663@haw-hamburg.de>
2024-08-10 23:25:02 +0200

    Hakan Bostan, Cpt CC A258, 10.08.2024

--- captainciaociao/src/main/java/kapitelzwei/uebungZweiFuenfAcht.java
+++ captainciaociao/src/main/java/kapitelzwei/uebungZweiFuenfAcht.java
@@ -2,7 +2,7 @@ package kapitelzwei;
 
 /**
  * 
- * Die Methode 
+ * Die Methode
  * 
  * @param int n
  * @return n.A.
@@ -10,8 +10,49 @@ package kapitelzwei;
 
 public class uebungZweiFuenfAcht {
 
-    public void a258(){
-        System.out.println("Hayat nicht fertig geworden ");
+    public void a258() {
+
+        double zahl = -12345;
+        double zahlZifferCount = zahl;
+        int zahldiv = (int) zahl;
+
+        int counter = 0;
+
+        int kleinsteziffer = 9;
+        int grosteziffer = 0;
+
+        // schleife um herauszufinden wie oft man interieren muss.
+        while (zahlZifferCount >= 1) {
+            zahlZifferCount /= 10;
+            counter++;
+        }
+
+        // für den Fall das für Zahl '0' eingegeben wird, kommt das Wunschergebnis raus.
+        if (zahldiv == 0) {
+            kleinsteziffer = 0;
+        }
+
+        // schleife um kleinste % größte Ziffer herauszufinden
+
+        for (int i = 0; i < counter; i++) {
+
+            {
+                int temp = zahldiv % 10;
+                zahldiv = zahldiv / 10;
+
+                if (temp <= kleinsteziffer) {
+                    kleinsteziffer = temp;
+                }
+
+                if (temp >= grosteziffer) {
+                    grosteziffer = temp;
+                }
+
+            }
+
+        }
+
+        System.out.println(kleinsteziffer + ", " + grosteziffer);
     }
 
 }
