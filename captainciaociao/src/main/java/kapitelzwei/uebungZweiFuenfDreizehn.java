8fe1078b8b40220bede085c60b1401b33f22ba68
Hakan Bostan <wua663@haw-hamburg.de>
2024-08-17 23:56:03 +0200

    Hakan Bostan, Cpt CC A2513, 17.08.2024

--- captainciaociao/src/main/java/kapitelzwei/uebungZweiFuenfDreizehn.java
+++ captainciaociao/src/main/java/kapitelzwei/uebungZweiFuenfDreizehn.java
@@ -3,7 +3,7 @@ package kapitelzwei;
 public class uebungZweiFuenfDreizehn {
     
     public static void main(String[] args) {
-        System.out.println("Damit alles grün ist");
+        System.out.println("Damit alles grün ist teil Zwei");
     }
     
 }
